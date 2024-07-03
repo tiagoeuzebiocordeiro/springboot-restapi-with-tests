53edfad48ce02bf107b1c5451a669d4aaa9a82b9
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-07-02 22:08:50 -0300

    User entity created with lombok annotations

---
+++ src/main/java/com/tiagocordeiro/apirest/domain/User.java
@@ -0,0 +1,22 @@
+package com.tiagocordeiro.apirest.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
