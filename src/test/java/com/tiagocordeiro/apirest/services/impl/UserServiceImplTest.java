844843d38cdac023c315ec1a7c073a5b504da1da
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-07-10 20:17:32 -0300

    UserService deleteWithSuccess test

--- src/test/java/com/tiagocordeiro/apirest/services/impl/UserServiceImplTest.java
+++ src/test/java/com/tiagocordeiro/apirest/services/impl/UserServiceImplTest.java
@@ -159,7 +159,12 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void whenDeleteThenReturnSuccess() {
+        Mockito.when(repository.findById(Mockito.anyInt())).thenReturn(optionalUser);
+
+        Mockito.doNothing().when(repository).deleteById(Mockito.anyInt());
+        service.delete(ID);
+        Mockito.verify(repository, Mockito.times(1)).deleteById(Mockito.anyInt());
     }
 
     private void initUser() {
